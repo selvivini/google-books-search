77bedf64757c328ea4a0a516ce12b905ea62c18f
vinitha <selvivini@gmail.com>
2021-05-06 16:23:32 -0500

    removed the boiler plate added packages and server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
