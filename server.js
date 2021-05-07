6fb771995127c7c846279f9095d8667e6f86c94a
vinitha <selvivini@gmail.com>
2021-05-07 14:23:40 -0500

    refactoring the folder structure

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-books-search");
 
 // Start the API server
 app.listen(PORT, function() {
