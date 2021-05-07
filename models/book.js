63b4000316092628092784ad6538a4ec31d0a669
vinitha <selvivini@gmail.com>
2021-05-07 15:29:02 -0500

    addedboiler plate for models, controllers and routes

---
+++ models/book.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  volumeInfo  : { 
+    title       : { type: String, required: true },
+    authors     : [ { type: String, required: true } ],
+    description : String,
+    image  : String,
+    link    : String,
+    date: {default: Date.now}
+  }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
