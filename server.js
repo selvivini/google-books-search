55b2bbb1e6047626e8d5cf2486594417675d5da6
vinitha <selvivini@gmail.com>
2021-05-07 12:31:52 -0500

    moved the public and src folder to client

--- server.js
+++ server.js
@@ -11,3 +11,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
