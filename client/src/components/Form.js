534062427704d39c4ff02044bb62c33d0a6145f3
vinitha <selvivini@gmail.com>
2021-05-10 12:58:12 -0500

    worked on serach component using a class

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,11 +1,35 @@
-import React from 'react'
+  
+import React from "react";
 
-function Form() {
-    return (
-        <div>
-            <h1>Books Search form</h1>
-        </div>
-    )
+function Form({ q, handleInputChange, handleFormSubmit }) {
+  return (
+    <form>
+      <div className="form-group">
+        <label htmlFor="Query">
+          <strong>Book</strong>
+        </label>
+        <input
+          className="form-control"
+          id="Title"
+          type="text"
+          value={q}
+          placeholder="Ready Player One"
+          name="q"
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <div className="pull-right">
+        <button
+          onClick={handleFormSubmit}
+          type="submit"
+          className="btn btn-lg btn-danger float-right"
+        >
+          Search
+        </button>
+      </div>
+    </form>
+  );
 }
 
-export default Form
+export default Form;
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,19 +1,94 @@
-import React from 'react'
-import{Container} from 'react-bootstrap'
+import React, {Component} from 'react'
+import axios from 'axios'
+import{Container, Row, Col, Card, Button} from 'react-bootstrap'
 import NavBar from '../components/Nav'
 import Jumbo from '../components/Jumbotron/Jumbo'
-function Search() {
-    return (
-        <Container fluid>
-           <NavBar/>
-           <Jumbo/>
-           <h4 className="text-center m-3 p-3 text-danger">Search and save books of interest</h4>
-        </Container>
-         
-        
-    )
+
+import Form from '../components/Form'
+import Api from '../utils/Api'
+import SearchResult from '../components/SearchResult'
+class Search extends Component {
+ //create state
+ state = {
+  search: "",
+  books: [],
+  error: "",
+  message: ""
+};
+
+ 
+
+  handleInputChange = event => {
+    this.setState({ search: event.target.value })
+  };
+  
+
+  //function to control the submit button of the search form 
+  handleFormSubmit = event => {
+    event.preventDefault();
+    // once it clicks it connects to the google book api with the search value
+    Api.getGoogleSearchBooks(this.state.search)
+        .then(res => {
+            if (res.data.items === "error") {
+                throw new Error(res.data.items);
+            }
+            else {
+                // store response in a array
+                let results = res.data.items
+                //map through the array 
+                results = results.map(result => {
+                    //store each book information in a new object 
+                    result = {
+                        key: result.id,
+                        id: result.id,
+                        title: result.volumeInfo.title,
+                        author: result.volumeInfo.authors,
+                        description: result.volumeInfo.description,
+                        image: result.volumeInfo.imageLinks.thumbnail,
+                        link: result.volumeInfo.infoLink
+                    }
+                    return result;
+                })
+                // reset the sate of the empty books array to the new arrays of objects with properties geting back from the response
+                this.setState({ books: results, error: "" })
+            }
+        })
+        .catch(err => this.setState({ error: err.items }));
 }
 
-export default Search
 
+handleSavedButton = event => {
+  // console.log(event)
+  event.preventDefault();
+  console.log(this.state.books)
+  let savedBooks = this.state.books.filter(book => book.id === event.target.id)
+  savedBooks = savedBooks[0];
+  Api.saveBook(savedBooks)
+      .then(this.setState({ message: alert("Your book is saved") }))
+      .catch(err => console.log(err))
+}
+   
+  render() {
+    return (
+      <Container fluid>
+        <NavBar/>
+        <Jumbo/>
+        <Row>
+         <Col >
+            <Card title="Book Search" icon="far fa-book">
+              <Form
+                handleInputChange={this.handleInputChange}
+                handleFormSubmit={this.handleFormSubmit}
+              />
+            </Card>
+          </Col>
+        </Row>
+       <SearchResult books= {this.state.books} handleSavedButton={this.handleSavedButton}/>
+        
+      </Container>
+    );
+  }
+}
+
+export default Search;
 
